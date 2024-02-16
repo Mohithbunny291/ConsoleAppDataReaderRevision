d5085cfc9e3d9cd867cfffc28124ec951123925d
Mohith Reddy <Mohithbunny291@gmail.com>
2024-02-16 10:45:26 +0530

    Add project files.

---
+++ ConsoleAppDataReaderRevision/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace ConsoleAppDataReaderRevision
+{
+    internal class Program
+    {
+        static SqlConnection con;
+        static SqlCommand cmd;
+        static SqlDataReader sqlDataReader;
+        static string conStr = "server=mohith-reddy-el;database=EmployeesDb;trusted_conniction=true";
+        static void Main(string[] args)
+        {
+            try
+            {
+                con = new SqlConnection(conStr);
+                cmd = new SqlCommand("Select * from Employees", con);
+                con.Open();
+                sqlDataReader = cmd.ExecuteReader();
+                while (sqlDataReader.Read())
+                {
+                    Console.WriteLine("Id: " + sqlDataReader["Id"]);
+                    Console.WriteLine("Fname: " + sqlDataReader["Fname"]);
+                    Console.WriteLine("Lname: " + sqlDataReader["Lname"]);
+                    Console.WriteLine("Designation: " + sqlDataReader["Designation"]);
+                    Console.WriteLine("Salary: " + sqlDataReader["Salary"]);
+                    Console.WriteLine("Doj: " + sqlDataReader["Doj"]);
+                    Thread.Sleep(1000);
+                    Console.WriteLine("\n");
+                }
+            }catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }finally { con.Close(); }
+        }
+    }
+}
---
+++ ConsoleAppDataReaderRevision/SqlDataAdapterRevision.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace ConsoleAppDataReaderRevision
+{
+    internal class SqlDataAdapterRevision
+    {
+        static SqlConnection con;
+        static SqlCommand cmd;
+        static SqlDataAdapter sqlDataAdapter;
+        static string conStr = "Data Source=mohith-reddy-el;Initial Catalog=EmployeesDb;Integrated Security=True;TrustServerCertificate=True";
+        static DataSet dataSet;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                con = new SqlConnection(conStr);
+                cmd = new SqlCommand("Select * from Employees", con);
+                con.Open();
+                sqlDataAdapter = new SqlDataAdapter(cmd);
+                dataSet = new DataSet();
+                sqlDataAdapter.Fill(dataSet);
+                con.Close();
+                Console.WriteLine("ID \t FirstName \t LastName \t\t Designation \t Salary \t DOJ");
+                foreach (DataRow dr in dataSet.Tables[0].Rows)
+                {
+                    Console.Write(dr["Id"] + "\t");
+                    Console.Write(dr["Fname"] + "\t\t");
+                    Console.Write(dr["Lname"] + "\t\t\t");
+                    Console.Write(dr["Designation"] + "\t\t");
+                    Console.Write(dr["Salary"] + "\t");
+                    Console.Write(dr["Doj"] + "\t");
+                    Console.WriteLine();
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally { con.Close(); }
+        }
+    }
+}
